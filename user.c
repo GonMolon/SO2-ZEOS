9c9ea9592ca4f129a3c679cf4b5136494ece8ca8
Gonzalo Solera <gonzalo1796@gmail.com>
2018-02-26 15:06:52 +0100

    Finished intro session

--- user.c
+++ user.c
@@ -24,9 +24,21 @@ long outer(long n) {
     return acum;
 }
 
+int add(int a, int b) {
+    return a + b;
+}
+
+int add2(int a, int b);
+
 int __attribute__ ((__section__(".text.main"))) main(void) {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
-
-  while(1) { }
+    long count, acum, result;
+    count = 75;
+    acum = 0;
+    acum = outer(count);
+    result = add(1, 2);
+    result = add2(1, 2);
+    while(1);
+    return 0;
 }
