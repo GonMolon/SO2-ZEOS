bf1f82c60ebe4c67642c743d088897b1119d6cf9
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-07 01:37:04 +0100

    Removing util.S and cleaning user code

--- user.c
+++ user.c
@@ -1,49 +1,12 @@
 #include <libc.h>
 
-char buff[24];
+char* buff;
 
 int pid;
 
-long inner(long n) {
-    int i;
-    long suma;
-    suma = 0;
-    for(i=0; i<n; i++) {
-        suma = suma + i; 
-    }
-    return suma;
-}
-
-long outer(long n) {
-    int i;
-    long acum;
-    acum = 0;
-    for(i=0; i<n; i++) {
-        acum = acum + inner(i);
-    }
-    return acum;
-}
-
-int add(int a, int b) {
-    return a + b;
-}
-
-int add2(int a, int b);
-
 int __attribute__ ((__section__(".text.main"))) main(void) {
-    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
-     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
-    long count, acum, result;
-    count = 75;
-    acum = 0;
-    acum = outer(count);
-    result = add(1, 2);
-    result = add2(1, 2);
-    if(write(0, 0, 0) == -1) {
-        if(errno == -2) {
-            while(1);
-        }
-    }
-    // while(1);
+    buff = "Hello World";
+    write(1, buff, strlen(buff));
+    while(1);
     return 0;
 }
