43bbde532516e0961879581a00de9a9597fddfc8
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-10 17:39:34 +0100

    Code testing sys call gettime

--- user.c
+++ user.c
@@ -2,16 +2,20 @@
 #include <types.h>
 
 char* buff;
+char time_buff[10];
 
 int pid;
 
 int __attribute__ ((__section__(".text.main"))) main(void) {
-    buff = "Hello World";
-    if(write(1, NULL, strlen(buff)) == -1) {
+
+    unsigned long time = gettime();
+    itoa(time, time_buff);
+    if(write(1, time_buff, strlen(time_buff)) == -1) {
         buff = "Error detected:\n";
         write(1, buff, strlen(buff));
         perror();
     }
+
     while(1);
     return 0;
 }
