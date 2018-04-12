2850650cb00b5bf7e1508e2b65a6901edf8d407f
Gonzalo Solera <gonzalo1796@gmail.com>
2018-04-12 18:36:21 +0200

    Fixing format

--- devices.c
+++ devices.c
@@ -6,10 +6,10 @@
 struct list_head blocked;
 
 int sys_write_console(char *buffer,int size) {
-  int i;
+ 
+    for(int i = 0; i < size; i++) {
+        printc(buffer[i]);
+    }
   
-  for(i=0; i<size; i++)
-    printc(buffer[i]);
-  
-  return size;
+    return size;
 }
