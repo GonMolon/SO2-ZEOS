83b176c5b6fd4f22db0e21b7662c579b2032c1e6
Gonzalo Solera <gonzalo1796@gmail.com>
2018-05-04 21:03:48 +0200

    Adding test code

--- user.c
+++ user.c
@@ -1,16 +1,8 @@
 #include <libc.h>
 #include <types.h>
 
-void test() {
-    write(1, "child\n", 6);
-    // exit(1);
-    return;
-}
-
 int __attribute__ ((__section__(".text.main"))) main(void) {
-    int stack[32];
-    clone(&test, &stack[32]);
-    write(1, "father\n", 7);
+    runjp();
     while(1);
     return 0;
 }
